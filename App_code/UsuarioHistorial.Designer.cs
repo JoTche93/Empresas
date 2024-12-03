a0a985522f2a12572ccfe2be00ce8da8d0c60682
John10Alvarezz <jalvarez2023@alu.uct.cl>
2024-12-03 15:31:24 -0300

    usuarios

---
+++ App_code/UsuarioHistorial.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace TestLogin.App_code
+{
+    partial class UsuarioHistorial
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UsuarioHistorial));
+            pictureBox2 = new PictureBox();
+            label5 = new Label();
+            groupBox3 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            label4 = new Label();
+            CerrarSesion = new GroupBox();
+            label3 = new Label();
+            pictureBox4 = new PictureBox();
+            Barra = new Panel();
+            label7 = new Label();
+            btnMinimizar = new PictureBox();
+            btnCerrar = new PictureBox();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            groupBox2 = new GroupBox();
+            ButtonPerfil = new Label();
+            ButtonHistorial = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            CerrarSesion.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            Barra.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            groupBox2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(157, 16);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(39, 38);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 41;
+            pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Mongolian Baiti", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(353, 79);
+            label5.Name = "label5";
+            label5.Size = new Size(199, 40);
+            label5.TabIndex = 44;
+            label5.Text = "Bienvenido";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(pictureBox2);
+            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox3.Location = new Point(295, 149);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(559, 227);
+            groupBox3.TabIndex = 43;
+            groupBox3.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(4, 59);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(549, 162);
+            dataGridView1.TabIndex = 32;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label4.Location = new Point(6, 25);
+            label4.Name = "label4";
+            label4.Size = new Size(155, 17);
+            label4.TabIndex = 0;
+            label4.Text = "Historial de Actividades";
+            // 
+            // CerrarSesion
+            // 
+            CerrarSesion.BackgroundImageLayout = ImageLayout.Zoom;
+            CerrarSesion.Controls.Add(label3);
+            CerrarSesion.Controls.Add(pictureBox4);
+            CerrarSesion.Location = new Point(774, 47);
+            CerrarSesion.Name = "CerrarSesion";
+            CerrarSesion.Size = new Size(89, 96);
+            CerrarSesion.TabIndex = 42;
+            CerrarSesion.TabStop = false;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label3.Location = new Point(0, 66);
+            label3.Name = "label3";
+            label3.Size = new Size(89, 17);
+            label3.TabIndex = 33;
+            label3.Text = "Cerrar Sesión";
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(26, 11);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(41, 52);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 32;
+            pictureBox4.TabStop = false;
+            // 
+            // Barra
+            // 
+            Barra.BackColor = Color.Teal;
+            Barra.Controls.Add(label7);
+            Barra.Controls.Add(btnMinimizar);
+            Barra.Controls.Add(btnCerrar);
+            Barra.Location = new Point(0, 0);
+            Barra.Name = "Barra";
+            Barra.Size = new Size(866, 38);
+            Barra.TabIndex = 37;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(3, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(125, 16);
+            label7.TabIndex = 27;
+            label7.Text = "Empower Usuarios";
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Image = (Image)resources.GetObject("btnMinimizar.Image");
+            btnMinimizar.Location = new Point(784, 0);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(40, 38);
+            btnMinimizar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnMinimizar.TabIndex = 26;
+            btnMinimizar.TabStop = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.BackgroundImageLayout = ImageLayout.Zoom;
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(826, 0);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(40, 38);
+            btnCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnCerrar.TabIndex = 25;
+            btnCerrar.TabStop = false;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(3, 58);
+            label6.Name = "label6";
+            label6.Size = new Size(222, 42);
+            label6.TabIndex = 39;
+            label6.Text = "EMPOWER";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(183, 21);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 100);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 40;
+            pictureBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(ButtonPerfil);
+            groupBox2.Controls.Add(ButtonHistorial);
+            groupBox2.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(26, 149);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(199, 123);
+            groupBox2.TabIndex = 38;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Opciones";
+            // 
+            // ButtonPerfil
+            // 
+            ButtonPerfil.AutoSize = true;
+            ButtonPerfil.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold);
+            ButtonPerfil.Location = new Point(6, 37);
+            ButtonPerfil.Name = "ButtonPerfil";
+            ButtonPerfil.Size = new Size(127, 17);
+            ButtonPerfil.TabIndex = 0;
+            ButtonPerfil.Text = "1. Ver/Editar Perfil";
+            // 
+            // ButtonHistorial
+            // 
+            ButtonHistorial.AutoSize = true;
+            ButtonHistorial.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold);
+            ButtonHistorial.Location = new Point(6, 74);
+            ButtonHistorial.Name = "ButtonHistorial";
+            ButtonHistorial.Size = new Size(174, 17);
+            ButtonHistorial.TabIndex = 2;
+            ButtonHistorial.Text = "2. Historial de Actividades";
+            // 
+            // UsuarioHistorial
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.CadetBlue;
+            ClientSize = new Size(866, 385);
+            Controls.Add(label5);
+            Controls.Add(groupBox3);
+            Controls.Add(CerrarSesion);
+            Controls.Add(Barra);
+            Controls.Add(label6);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox2);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "UsuarioHistorial";
+            Text = "UsuarioHistorial";
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            CerrarSesion.ResumeLayout(false);
+            CerrarSesion.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            Barra.ResumeLayout(false);
+            Barra.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private PictureBox pictureBox2;
+        private Label label5;
+        private GroupBox groupBox3;
+        private DataGridView dataGridView1;
+        private Label label4;
+        private GroupBox CerrarSesion;
+        private Label label3;
+        private PictureBox pictureBox4;
+        private Panel Barra;
+        private Label label7;
+        private PictureBox btnMinimizar;
+        private PictureBox btnCerrar;
+        private Label label6;
+        private PictureBox pictureBox1;
+        private GroupBox groupBox2;
+        private Label ButtonPerfil;
+        private Label ButtonHistorial;
+    }
+}
--- App_code/Usuarios.Designer.cs
+++ App_code/Usuarios.Designer.cs
@@ -28,7 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Usuarios));
+            Barra = new Panel();
+            label7 = new Label();
+            btnMinimizar = new PictureBox();
+            btnCerrar = new PictureBox();
+            groupBox2 = new GroupBox();
+            ButtonPerfil = new Label();
+            ButtonHistorial = new Label();
+            label6 = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            CerrarSesion = new Label();
+            groupBox1 = new GroupBox();
+            groupBox3 = new GroupBox();
+            pictureBox5 = new PictureBox();
+            label4 = new Label();
+            dataGridView1 = new DataGridView();
+            label5 = new Label();
+            Barra.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).BeginInit();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            groupBox1.SuspendLayout();
+            groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // Barra
+            // 
+            Barra.BackColor = Color.Teal;
+            Barra.Controls.Add(label7);
+            Barra.Controls.Add(btnMinimizar);
+            Barra.Controls.Add(btnCerrar);
+            Barra.Location = new Point(0, 0);
+            Barra.Name = "Barra";
+            Barra.Size = new Size(866, 38);
+            Barra.TabIndex = 26;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.Location = new Point(3, 9);
+            label7.Name = "label7";
+            label7.Size = new Size(125, 16);
+            label7.TabIndex = 27;
+            label7.Text = "Empower Usuarios";
+            // 
+            // btnMinimizar
+            // 
+            btnMinimizar.Image = (Image)resources.GetObject("btnMinimizar.Image");
+            btnMinimizar.Location = new Point(784, 0);
+            btnMinimizar.Name = "btnMinimizar";
+            btnMinimizar.Size = new Size(40, 38);
+            btnMinimizar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnMinimizar.TabIndex = 26;
+            btnMinimizar.TabStop = false;
+            // 
+            // btnCerrar
+            // 
+            btnCerrar.BackgroundImageLayout = ImageLayout.Zoom;
+            btnCerrar.Image = (Image)resources.GetObject("btnCerrar.Image");
+            btnCerrar.Location = new Point(826, 0);
+            btnCerrar.Name = "btnCerrar";
+            btnCerrar.Size = new Size(40, 38);
+            btnCerrar.SizeMode = PictureBoxSizeMode.Zoom;
+            btnCerrar.TabIndex = 25;
+            btnCerrar.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(ButtonPerfil);
+            groupBox2.Controls.Add(ButtonHistorial);
+            groupBox2.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox2.Location = new Point(26, 146);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(199, 123);
+            groupBox2.TabIndex = 27;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "Opciones";
+            // 
+            // ButtonPerfil
+            // 
+            ButtonPerfil.AutoSize = true;
+            ButtonPerfil.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold);
+            ButtonPerfil.Location = new Point(6, 37);
+            ButtonPerfil.Name = "ButtonPerfil";
+            ButtonPerfil.Size = new Size(127, 17);
+            ButtonPerfil.TabIndex = 0;
+            ButtonPerfil.Text = "1. Ver/Editar Perfil";
+            // 
+            // ButtonHistorial
+            // 
+            ButtonHistorial.AutoSize = true;
+            ButtonHistorial.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold);
+            ButtonHistorial.Location = new Point(6, 74);
+            ButtonHistorial.Name = "ButtonHistorial";
+            ButtonHistorial.Size = new Size(174, 17);
+            ButtonHistorial.TabIndex = 2;
+            ButtonHistorial.Text = "2. Historial de Actividades";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.Location = new Point(3, 55);
+            label6.Name = "label6";
+            label6.Size = new Size(222, 42);
+            label6.TabIndex = 28;
+            label6.Text = "EMPOWER";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(183, 18);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(125, 100);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 29;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.Image = (Image)resources.GetObject("pictureBox4.Image");
+            pictureBox4.Location = new Point(26, 11);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(41, 52);
+            pictureBox4.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox4.TabIndex = 32;
+            pictureBox4.TabStop = false;
+            // 
+            // CerrarSesion
+            // 
+            CerrarSesion.AutoSize = true;
+            CerrarSesion.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            CerrarSesion.Location = new Point(0, 66);
+            CerrarSesion.Name = "CerrarSesion";
+            CerrarSesion.Size = new Size(89, 17);
+            CerrarSesion.TabIndex = 33;
+            CerrarSesion.Text = "Cerrar Sesión";
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            groupBox1.Controls.Add(CerrarSesion);
+            groupBox1.Controls.Add(pictureBox4);
+            groupBox1.Location = new Point(774, 44);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(89, 96);
+            groupBox1.TabIndex = 34;
+            groupBox1.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(dataGridView1);
+            groupBox3.Controls.Add(pictureBox5);
+            groupBox3.Controls.Add(label4);
+            groupBox3.Font = new Font("Segoe UI Black", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            groupBox3.Location = new Point(295, 146);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(559, 227);
+            groupBox3.TabIndex = 35;
+            groupBox3.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.Image = (Image)resources.GetObject("pictureBox5.Image");
+            pictureBox5.Location = new Point(110, 15);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(41, 39);
+            pictureBox5.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox5.TabIndex = 31;
+            pictureBox5.TabStop = false;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold);
+            label4.Location = new Point(6, 25);
+            label4.Name = "label4";
+            label4.Size = new Size(107, 17);
+            label4.TabIndex = 0;
+            label4.Text = "Ver/Editar Perfil";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(6, 63);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(549, 162);
+            dataGridView1.TabIndex = 32;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Mongolian Baiti", 27.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(353, 76);
+            label5.Name = "label5";
+            label5.Size = new Size(199, 40);
+            label5.TabIndex = 36;
+            label5.Text = "Bienvenido";
             // 
             // Usuarios
             // 
@@ -36,11 +239,52 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.CadetBlue;
             ClientSize = new Size(866, 385);
+            Controls.Add(label5);
+            Controls.Add(groupBox3);
+            Controls.Add(groupBox1);
+            Controls.Add(Barra);
+            Controls.Add(label6);
+            Controls.Add(pictureBox1);
+            Controls.Add(groupBox2);
+            FormBorderStyle = FormBorderStyle.None;
             Name = "Usuarios";
             Text = "Form1";
+            Barra.ResumeLayout(false);
+            Barra.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)btnMinimizar).EndInit();
+            ((System.ComponentModel.ISupportInitialize)btnCerrar).EndInit();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Panel Barra;
+        private Label label7;
+        private PictureBox btnMinimizar;
+        private PictureBox btnCerrar;
+        private GroupBox groupBox2;
+        private Label ButtonPerfil;
+        private Label ButtonHistorial;
+        private Label label6;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox4;
+        private Label CerrarSesion;
+        private GroupBox groupBox1;
+        private GroupBox groupBox3;
+        private DataGridView dataGridView1;
+        private PictureBox pictureBox5;
+        private Label label4;
+        private Label label5;
     }
 }
